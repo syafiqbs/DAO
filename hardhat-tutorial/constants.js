1b345f00a7e2d498da8da6f96e3b10adb214fee4
syafiqbs <cross.symbol@hotmail.com>
2022-06-17 11:22:38 +0800

    Move files into repo

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x35913a47e481D3207568FBa2B0F9816375332d3D";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
